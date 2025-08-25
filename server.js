ebe4236cd5301e7b8bbe87902b47a01927762bef
Philscent Mark Otod <philscent09@gmail.com>
2025-08-25 13:07:59 +0800

    refactor: Use CLOUDINARY_URL for configuration

--- server.js
+++ server.js
@@ -39,20 +39,13 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'GovDocsRepo', // This will create a folder in your Cloudinary account
-        format: async (req, file) => 'pdf', // Or other formats like 'png', 'jpg'
-        public_id: (req, file) => file.originalname.split('.')[0] + '-' + Date.now(),
-    },
-});
+        cloudinary: cloudinary,
+        params: {
+            folder: 'GovDocsRepo', // A folder name in your Cloudinary account
+            public_id: (req, file) => file.originalname.split('.')[0] + '-' + Date.now(),
+        },
+    });
 
 const upload = multer({ storage: storage });
 
